a014e10e43f96f860833a253f397358dd9a43933
AbdelGaye <abdel.gaye@mail.mcgill.ca>
2018-02-27 19:23:37 -0500

    Facilitating the automatization of pcks

--- keypointscompare.m
+++ keypointscompare.m
@@ -1,12 +1,28 @@
+%Getting the image from the dataset
+image1 = '20130614_124600.png';
+
+%Getting the name of the .mat file of that image
+tf = strsplit(image1,'.');
+matpath = char(strcat(tf(1,1), '_P.mat/'));
+
+% Read the reference image containing the object of interest.
+picture = imread(image1);
+
+keyPoints = detectSURFFeatures(picture);
+
+[features, keyPoints] = extractFeatures(picture, keyPoints);
+
 %Sorting the keypoints
 %SURF
-pin = transpose(scenePoints.Location)
-
-pout = sortrows(pin',1)'
+pin = transpose(keyPoints.Location);
+pout = sortrows(pin',1)';
 
 %DATASET
-tpout = sortrows(TFeatures',1)'
+m = matfile(matpath);
+tFeatures = m.TFeatures;
+tpout = sortrows(tFeatures',1)';
 
+%Initializing the number of correct matches
 correctMatches = 0;
 
 k = size(pout,2);
@@ -16,9 +32,9 @@ for j = 1:1:l
     %Look through surf points
     for i = 1:1:k
         %If x in dataset is within a value in surf points
-        if abs(tpout(1,j) - pout(1,i)) < 5
+        if abs(tpout(1,j) - pout(1,i)) < 4
             %Check if y in dataset is within a value in surf points
-            if abs(tpout(2,j) - pout(2,i)) < 5
+            if abs(tpout(2,j) - pout(2,i)) < 4
                 %Increment number of dataset points found by surf if x and
                 %y are close
                 correctMatches = correctMatches + 1;
