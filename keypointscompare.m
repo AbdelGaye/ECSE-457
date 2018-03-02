aad0cc8a0c5c59ac76b9f8aa723d57dacd4144dd
ChrisDiBetta <christopher.dibetta@mail.mcgill.ca>
2018-03-01 19:10:50 -0500

    Can now automatically evaluate inputs

--- keypointscompare.m
+++ keypointscompare.m
@@ -1,54 +1,60 @@
 directory = 'G:\train';
-folder = dir([directory '\*.png']);
+folder = dir('*.png');
 %Need to get only the name from folder struct
-imageset = getfield(folder, 'name');
+imageset = extractfield(folder, 'name');
 
-%Getting the image from the dataset
-image1 = '20130614_124600.png';
+pck = zeros(1, length(imageset));
 
-%Getting the name of the .mat file of that image
-tf = strsplit(image1,'.');
-matpath = char(strcat(tf(1,1), '_P.mat/'));
+for z = 1:1:length(imageset)   
 
-% Read the reference image containing the object of interest.
-picture = imread(image1);
+    image = char(imageset(z));
+    %Getting the name of the .mat file of that image
+    tf = strsplit(image,'.');
+    matpath = char(strcat(tf(1,1), '_P.mat/'));
 
-keyPoints = detectSURFFeatures(picture);
+    % Read the reference image containing the object of interest.
+    picture = imread(image);
 
-[features, keyPoints] = extractFeatures(picture, keyPoints);
+    keyPoints = detectSURFFeatures(picture);
 
-%Sorting the keypoints
-%SURF
-pin = transpose(keyPoints.Location);
-pout = sortrows(pin',1)';
+    [features, keyPoints] = extractFeatures(picture, keyPoints);
 
-%DATASET
-m = matfile(matpath);
-tFeatures = m.TFeatures;
-tpout = sortrows(tFeatures',1)';
+    %Sorting the keypoints
+    %SURF
+    pin = transpose(keyPoints.Location);
+    pout = sortrows(pin',1)';
 
-%Initializing the number of correct matches
-correctMatches = 0;
+    %DATASET
+    m = matfile(matpath);
+    tFeatures = m.TFeatures;
+    tpout = sortrows(tFeatures',1)';
 
-k = size(pout,2);
-l = size(tpout,2);
-%Look through dataset
-for j = 1:1:l
-    %Look through surf points
-    for i = 1:1:k
-        %If x in dataset is within a value in surf points
-        if abs(tpout(1,j) - pout(1,i)) < 4
-            %Check if y in dataset is within a value in surf points
-            if abs(tpout(2,j) - pout(2,i)) < 4
-                %Increment number of dataset points found by surf if x and
-                %y are close
-                correctMatches = correctMatches + 1;
-                break;
-            end    
+    k = size(pout,2);
+    l = size(tpout,2);
+    %Look through dataset
+    
+    %Initializing the number of correct matches
+    correctMatches = 0;
+    
+    for j = 1:1:l
+        %Look through surf points
+        for i = 1:1:k
+            %If x in dataset is within a value in surf points
+            if abs(tpout(1,j) - pout(1,i)) < 4
+                %Check if y in dataset is within a value in surf points
+                if abs(tpout(2,j) - pout(2,i)) < 4
+                    %Increment number of dataset points found by surf if x and
+                    %y are close
+                    correctMatches = correctMatches + 1;
+                    break;
+                end    
+            end
         end
     end
+
+
+    pck(z) = correctMatches / size(tpout,2);
 end
 
 
-pck = correctMatches / size(tpout,2);
 
